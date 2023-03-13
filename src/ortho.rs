37aa418473a152221d0629748db482beef90ece1
Brendan Bortz <brendansbortz@protonmail.com>
2023-03-13 16:36:52 -0400

    b

--- src/ortho.rs
+++ src/ortho.rs
@@ -20,22 +20,27 @@ impl Ortho {
     }
 }
 
-impl Mul<(Vec2, f32, f32)> for Ortho {
-    type Output = (Vec2, f32, f32);
+impl Mul<((Vec2, f32), f32)> for Ortho {
+    type Output = ((Vec2, f32), f32);
 
-    fn mul(self, (rhs, z, w): (Vec2, f32, f32)) -> (Vec2, f32, f32) {
+    fn mul(self, ((rhs, z), w): ((Vec2, f32), f32)) -> ((Vec2, f32), f32) {
         (
-            Vec2::new(
-                self.0[0][0] * rhs.x()
-                    + self.0[0][1] * rhs.y()
-                    + self.0[0][2] * z
-                    + self.0[0][3] * w,
-                self.0[1][0] * rhs.x()
-                    + self.0[1][1] * rhs.y()
-                    + self.0[1][2] * z
-                    + self.0[1][3] * w,
+            (
+                Vec2::new(
+                    self.0[0][0] * rhs.x()
+                        + self.0[0][1] * rhs.y()
+                        + self.0[0][2] * z
+                        + self.0[0][3] * w,
+                    self.0[1][0] * rhs.x()
+                        + self.0[1][1] * rhs.y()
+                        + self.0[1][2] * z
+                        + self.0[1][3] * w,
+                ),
+                self.0[2][0] * rhs.x()
+                    + self.0[2][1] * rhs.y()
+                    + self.0[2][2] * z
+                    + self.0[2][3] * w,
             ),
-            self.0[2][0] * rhs.x() + self.0[2][1] * rhs.y() + self.0[2][2] * z + self.0[2][3] * w,
             self.0[3][0] * rhs.x() + self.0[3][1] * rhs.y() + self.0[3][2] * z + self.0[3][3] * w,
         )
     }
